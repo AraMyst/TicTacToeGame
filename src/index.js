b35c03294cfbbf5ef22020a118eeb499b0b447c1
Karla de Matos <kcristinardematos@gmail.com>
2025-05-10 10:03:01 +0200

    Switch to Game component to enable time travel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// src/index.js
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import Game from './Game';      // Import the Game component with time-travel
 import './index.css';
 
 const container = document.getElementById('root');
@@ -8,6 +10,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
